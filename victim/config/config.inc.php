db839095c5ca1460b8ec3a4200eab944197c0c62
HazemYM02 <hazem7277@gmail.com>
2025-07-10 22:26:00 +0300

    Initial commit: Cyber Range Emulator with DVWA victims

---
+++ victim/config/config.inc.php
@@ -0,0 +1,13 @@
+<?php
+$_DVWA = array();
+$_DVWA[ 'db_server' ]   = 'mariadb';
+$_DVWA[ 'db_database' ] = 'dvwa';
+$_DVWA[ 'db_user' ]     = 'root';
+$_DVWA[ 'db_password' ] = 'password';
+$_DVWA[ 'recaptcha_public_key' ]  = '';
+$_DVWA[ 'recaptcha_private_key' ] = '';
+$_DVWA[ 'default_security_level' ] = 'low';
+$_DVWA[ 'default_phpids_level' ] = 0;
+$_DVWA[ 'default_http_header_level' ] = 'medium';
+$_DVWA[ 'enable_phpids' ] = false;
+?>
--- victim/startup.sh
+++ victim/startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ip route add 172.20.0.0/24 via 172.22.0.1
 ip route add 172.21.0.0/24 via 172.22.0.1
+apache2ctl -D FOREGROUND
 exec "$@"
